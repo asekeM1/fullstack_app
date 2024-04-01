94d3bc2f37d29f71be1c03574780e0a2d05d8d7a
aseke <asanmaratulu@gmail.com>
2024-04-02 03:11:28 +0500

    Handling POST Requests (Django REST)

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateJoinPage from "./CreateRoomPage";
 
 export default class App extends Component{
     constructor(props){
@@ -7,9 +10,13 @@ export default class App extends Component{
     }
 
     render(){
-        return <h1>hello from react</h1>;
+        return(
+            <div>
+                <HomePage></HomePage>
+            </div>
+        );
     }
 }
 
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 render(<App/>, appDiv);
---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class CreateJoinPage extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <p>This is CreateJoinPage</p>
+    }
+}
